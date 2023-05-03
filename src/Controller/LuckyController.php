00a5683d6700adfd23d332e979557fa2424decf5
dwcs_code_2223 <dwcs.code.2223@gmail.com>
2023-05-03 20:24:25 +0200

    Actividad 7.1

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route(
+        '/lucky/number/{max<\d+>}',
+        name: 'app_lucky'
+    )]
+    public function index(int $max): Response
+    {
+
+        $number = random_int(0, $max);
+
+
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+            'maximo' => $max,
+            'numero' => $number
+        ]);
+    }
+    #[Route('/lucky/producto/{a<\d+>}/{b<\d+>}', name: 'app_lucky_producto')]
+    public function producto(int $a, int $b): Response
+    {
+        $producto = $a * $b;
+
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+            'maximo' => '',
+            'numero' => '',
+            'a' => $a,
+            'b' => $b,
+            'resultado' => $producto
+
+        ]);
+    }
+}
