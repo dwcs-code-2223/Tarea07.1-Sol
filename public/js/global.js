2d482951aea9ce469a3d1d4149e1be1445f8f38f
dwcs_code_2223 <dwcs.code.2223@gmail..com>
2023-05-23 09:51:56 +0200

    ++ global.js comments

--- public/js/global.js
+++ public/js/global.js
@@ -1,4 +1,7 @@
 const BASE_URL = "http://localhost:8000";
+//es posible que tengáis que adecuarla a la url que estáis viendo en vuestro navegador
+//No es lo mismo https://localhost:8000 ni http://127.0.0.1:8000
+//Comprobad que sea la misma URL que veis en el navegador para no tener que hacer configuración adicional de CORS: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 
 
 
